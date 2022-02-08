a204546c47fee73957e0e5870b54210677d0c120
Andrey <galan.andrey@gmail.com>
2022-02-08 20:22:38 +0300

    z2.2

--- lab1/z2.1.rb
+++ lab1/z2.1.rb
@@ -1,7 +1,25 @@
-number = ARGV[0].to_i
-digitsSum = 0
-while number > 0
-    digitsSum += number % 10
-    number /= 10
+def getSumDigits
+    number = ARGV[0].to_i
+    digitsSum = 0
+    while number > 0
+        digitsSum += number % 10
+        number /= 10
+    end
+    digitsSum
 end
-puts digitsSum.to_s
+
+def getMaxDigit nubmer
+    nubmer.digits.max
+end
+
+def getMinDigit nubmer
+    nubmer.digits.min
+end
+
+def getMulDigit nubmer
+    digitsMul = 1
+    nubmer.digits.each{|i| digitsMul *= i }
+    digitsMul
+end
+
+puts getMulDigit(273)
