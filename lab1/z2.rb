73b5b9e8d6519e88c05fba1084bea8a9e2637f09
Andrey <galan.andrey@gmail.com>
2022-02-08 19:13:30 +0300

    z1.3.1

--- lab1/z2.rb
+++ lab1/z2.rb
@@ -5,3 +5,18 @@ else
 end
 
 puts String.methods
+
+puts "Введите ваш любимый язык: "
+answer = STDIN.gets.chomp
+case answer
+    when "ruby"
+        puts "Подлиза"
+    when "python"
+        puts "Переключайтесь на ruby"
+    when "c++"
+        puts "Хороший язык, молодец. Но ruby скоро станет вышим любимым"
+    when "c#"
+        puts "Хороший язык, но ruby лучше"
+    else
+        puts "В любом случае, скоро будет  ruby"
+end
