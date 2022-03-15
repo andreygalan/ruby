a4cb7d03f8dd45228f1dec6ed09015884cd64fbc
Andrey <galan.andrey@gmail.com>
2022-03-15 20:47:24 +0300

    z4 method2

--- lab1/z4.rb
+++ lab1/z4.rb
@@ -1,9 +1,17 @@
 def method1 array
     array.inject(0){|min_index,i| i< array[min_index] ? i : min_index}
 end
+
 def method1_v2 array
     _, idx = array.each_with_index.min
     idx
 end
 
-puts method1_v2 [8,3,4,5,1,6,7]
+def method2 a,b
+    b-a+1
+end
+
+def method2_v2 array,a,b
+    array[a..b].size
+end
+puts method2 1,5
