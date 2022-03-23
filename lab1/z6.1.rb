a2a3adf30d59e078a4096fe1eb6ed2d2fcbb3b87
deadraider <galan.andrey@gmail.com>
2022-03-23 14:23:42 +0300

    z6.2

--- lab1/z6.1.rb
+++ lab1/z6.1.rb
@@ -1,8 +1,6 @@
 def read_arrayf(path)
-
     input = File.open(path)
-    input.readlines.map {|line| line.strip}
-       
+    input.readlines.map {|line| line.strip}      
 end
 
 puts read_arrayf('stringFILE.txt').sort_by {|line| line.size}
---
+++ lab1/z6.2.rb
@@ -0,0 +1,5 @@
+def read_arrayf(path)
+    input = File.open(path)
+    input.readlines.map {|line| line.strip}    
+end
+puts read_arrayf('stringFILE.txt').sort_by {|line| line.split.size}
