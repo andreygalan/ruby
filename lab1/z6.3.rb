6fbf567db04fc3bf7c1879d64dbf785eb5f2db8a
deadraider <galan.andrey@gmail.com>
2022-03-23 15:22:51 +0300

    z6.3 method2

--- lab1/z6.3.rb
+++ lab1/z6.3.rb
@@ -4,9 +4,14 @@ def read_arrayf(path)
 end
 
 def sort_method1(lines)
-    lines.sort_by {|line| line.chars.inject(0){|ascii_sum, char| ascii_sum +=char.ord}/line.chars.size }
+    lines.sort_by {|line| line.chars.inject(0){|ascii_sum, char| ascii_sum += char.ord}/line.chars.size }
+end
+
+def sort_method2(lines)
+    avr_ascii = lines[0].chars.inject(0){|ascii_sum, char| ascii_sum +=char.ord}/lines[0].chars.size
+    lines.sort_by {|line| (line.chars.inject(0){|ascii_sum, char| ascii_sum += char.ord}/line.chars.size - avr_ascii)**2 }
 end
 
 lines = read_arrayf('stringFILE.txt')
 
-puts sort_method1(lines)
+puts sort_method2(lines)
