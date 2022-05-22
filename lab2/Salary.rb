429c4e4e9583f1294a7cbb6e3065db5a574fa1b0
deadraider <galan.andrey@gmail.com>
2022-05-22 20:07:57 +0300

    percent_sal

--- lab2/Salary.rb
+++ lab2/Salary.rb
@@ -29,5 +29,34 @@ class Rub_sal < Fix_sal
     
 end
 
+class Percent_sal < Fix_sal
 
+    def initialize(fixed,add_percent)
+        super(fixed)
+        @add_percent=add_percent      
+    end
+
+    def get_salary
+            fixed = super
+        if rand(2)==1
+            fixed + fixed * @add_percent
+        else
+            fixed 
+        end
+
+    end
+end
+
+class Rub_percent_sal < Percent_sal
+
+    def initialize(fixed,add_percent,add_rub)
+        super(fixed,add_percent)
+        @add_rub = add_rub
+        
+    end
+
+    def get_salary
+        super + @add_rub
+    end
+end
 
